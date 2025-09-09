dadbd9e73018295304963d754d7fb5b8ae831aa9
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-09-09 15:06:37 +0700

    chore: add server for document api

--- auth-service/src/main/java/com/banking_app/auth_service/infrastructure/config/SwaggerConfig.java
+++ auth-service/src/main/java/com/banking_app/auth_service/infrastructure/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +15,8 @@ import org.springframework.context.annotation.Configuration;
         @Info(
             title = "Auth Service API",
             version = "1.0.0",
-            description = "API documentation for Auth Service"))
+            description = "API documentation for Auth Service"),
+    servers = {@Server(url = "/"), @Server(url = "/auth-service")})
 @SecurityScheme(
     name = "Bearer Authentication",
     type = SecuritySchemeType.HTTP,
