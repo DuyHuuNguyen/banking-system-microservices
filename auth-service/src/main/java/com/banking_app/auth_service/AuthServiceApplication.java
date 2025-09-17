174ad984d9dab439bfe55db7bb78d97290b2e5d7
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-09-17 14:04:54 +0700

    intergation euraka

--- auth-service/src/main/java/com/banking_app/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/banking_app/auth_service/AuthServiceApplication.java
@@ -1,19 +1,12 @@
 package com.banking_app.auth_service;
 
-import jakarta.annotation.PostConstruct;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceApplication {
-  Logger logger = LogManager.getLogger(AuthServiceApplication.class);
-
-  @PostConstruct
-  void log() {
-    logger.info("run application james demo log4j");
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(AuthServiceApplication.class, args);
